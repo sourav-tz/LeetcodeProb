abedad9af8cd53ef8ebbb51993ed7d06918e7dba
Sourav_Oz <boysouravs833588@gmail.com>
2026-01-14 21:05:26 +0530

    add next permu

---
+++ Arrays/mid/nextPermutation.cpp
@@ -0,0 +1,28 @@
+// LeetCode 31 - Next Permutation
+// https://leetcode.com/problems/next-permutation/
+
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int index=-1;
+        int n = nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1){
+            return reverse(nums.begin(),nums.end());
+        }
+
+        for(int i= n-1;i>=index;i--){
+            if(nums[i]>nums[index]){
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+        return reverse(nums.begin()+index+1,nums.end());
+    }
+};
